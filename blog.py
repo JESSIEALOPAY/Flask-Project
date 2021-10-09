0467a36300dc33eb0554b9dc416be30a550de4b7
Jessie Alopay <burak.begins@gmail.com>
2021-10-09 22:33:34 +0300

    New Many processes

--- blog.py
+++ blog.py
@@ -1,8 +1,20 @@
+from MySQLdb import cursors
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from flask_mysqldb import MySQL
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from wtforms import Form, StringField, TextAreaField, PasswordField, form, validators
 from passlib.hash import sha256_crypt
+from functools import wraps
 
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "logged_in" in session:
+            return f(*args, **kwargs)
+        else:
+            flash("İf you want see this page, you should log in","danger")
+            return redirect("login")
+    return decorated_function
 
 class RegisterForm(Form):
     name = StringField("name surname", validators=[validators.DataRequired(message="You can not space to name"), validators.Length(
@@ -15,6 +27,10 @@ class RegisterForm(Form):
         validators.EqualTo(fieldname="confirm")
         ])
     confirm=PasswordField("verify password")
+
+class LogInForm(Form):
+    username=StringField("USERNAME",validators=[validators.DataRequired(message="please provide your username")])
+    password= PasswordField("PASSWORD",validators=[validators.DataRequired("Please provide ypur password")])
 
 app = Flask(__name__)
 app.secret_key = '_5#y2L"F4Q8zsaasf'
@@ -46,7 +62,7 @@ def about():
 
 
 @app.route("/articles/<string:id>")
-def articles(id):
+def articlesWithId(id):
     return f"Article id is {id}"
 
 @app.route("/register",methods=["GET","POST"])
@@ -64,10 +80,80 @@ def reqister():
         mysql.connection.commit()
         cursor.close()
         flash("congrats you has been registered","success")
-        return redirect(url_for("index"))
+        return redirect(url_for("login"))
+    return render_template("register.html",form=form)
+
+@app.route("/login",methods=["GET","POST"])
+def login():
+    form= LogInForm(request.form)
+    if request.method=="POST" and form.validate():
+        username= form.username.data
+        password= form.password.data
+        queryString="select * from users where username = %s"
+        cursor= mysql.connection.cursor()
+        result= cursor.execute(queryString,(username,)) 
+        if result>0:
+            data= cursor.fetchone()
+            realpassword= data["password"]
+            if sha256_crypt.verify(password,realpassword):
+                flash("you have successfully logged in","success")
+                session["logged_in"]=True
+                session["user"]=username
+                return redirect(url_for("index"))
+            else:
+                flash("Your entered password is wrong","danger")    
+                return redirect(url_for("login"))
+
+        else:
+            flash("User has not found. Please check your inputs...","danger")
+            return redirect(url_for("login"))
+    return render_template("login.html",form=form)
+
+@app.route("/logout")
+def logout():
+    if not "logged_in" in session:
+        flash("you already been logged out","warning")
     else:
-        flash("Please check your inputs","danger")
-        return render_template("register.html",form=form)
+        session.clear()
+        flash("You have succesfully logged out","danger")
+    return redirect(url_for("index"))
+
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/addarticle",methods=["GET","POST"])
+@login_required
+def addarticle():
+    form=ArticleForm(request.form)
+    if request.method=="POST" and form.validate():
+        title= form.title.data
+        content= form.content.data
+        queryString="insert into articles(title,author,content) values(%s,%s,%s)"
+        cursor= mysql.connection.cursor()
+        cursor.execute(queryString,(title,session["user"],content))
+        mysql.connection.commit()
+        cursor.close()
+        flash("The Article has successfully added","success")
+        return redirect(url_for("dashboard"))
+    return render_template("addarticle.html",form=form)
+
+#Artcile Form
+class ArticleForm(Form):
+    title= StringField("ARTICLE HEADER",validators=[validators.Length(min=5,max=100)])
+    content= TextAreaField("CONTENT AREA",validators=[validators.Length(min=10)])
+
+
+@app.route("/articles")
+def articles():
+    cursor= mysql.connection.cursor()
+    result= cursor.execute("select * from articles")
+    if result>0:
+        articles=cursor.fetchall()
+        return render_template("articles.html",articles=articles)
+    else:
+        return render_template("articles.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
